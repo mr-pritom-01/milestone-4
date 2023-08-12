63a4b565a4a04cf0e4571916fd2e09d843582f8b
Pritom Das <pritom6221@gmail.com>
2023-08-12 20:48:58 +0600

    completed 1st one

--- new-matched.js
+++ new-matched.js
@@ -11,18 +11,28 @@
 
 const products = [
     {id: 1, name: 'Walton Phone', price: '20000'},
-    {id: 2, name: 'iPhone', price: '120000'},
+    {id: 2, name: 'Apple iPhone', price: '120000'},
     {id: 3, name: 'MacBook Pro', price: '220000'},
     {id: 4, name: 'Microsoft Surface Pro', price: '150000'},
-    {id: 5, name: 'Surface Pro 2', price: '170000'},
-    {id: 6, name: 'iPad Air', price: '140000'},
-    {id: 7, name: 'Nokia', price: '30000'},
+    {id: 5, name: 'Microsoft S. Pro 2', price: '170000'},
+    {id: 6, name: 'Apple iPad Air', price: '140000'},
+    {id: 7, name: 'Nokia Phone', price: '30000'},
+    {id: 8, name: 'Nokia Phone 3100', price: '10000'},
 ]
 
 // for(const product of products){
 //     console.log(product);
 // }
 
-function productMatcheds (products, search){
-    
+function mathcedProducts (products, search){
+    const matched = [];
+    for(const product of products){
+        if(product.name.toLowerCase().includes(search.toLowerCase())){
+            matched.push(product);
+        }
+    }
+    return matched;
 }
+
+const result = mathcedProducts(products, 'Phone');
+console.log(result);
