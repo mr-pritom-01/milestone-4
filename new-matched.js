de3201f4e43ac2c3bea36a415db640b0f8980408
Pritom Das <pritom6221@gmail.com>
2023-08-12 19:21:04 +0600

    function added

---
+++ new-matched.js
@@ -0,0 +1,28 @@
+// const numbers = [45, 65, 23, 98, 19];
+
+// for(let i = 0; i < numbers.length; i++){
+//     const number = numbers[i];
+//     console.log(number);
+// }
+
+// for(const number of numbers){
+//     console.log(number)
+// }
+
+const products = [
+    {id: 1, name: 'Walton Phone', price: '20000'},
+    {id: 2, name: 'iPhone', price: '120000'},
+    {id: 3, name: 'MacBook Pro', price: '220000'},
+    {id: 4, name: 'Microsoft Surface Pro', price: '150000'},
+    {id: 5, name: 'Surface Pro 2', price: '170000'},
+    {id: 6, name: 'iPad Air', price: '140000'},
+    {id: 7, name: 'Nokia', price: '30000'},
+]
+
+// for(const product of products){
+//     console.log(product);
+// }
+
+function productMatcheds (products, search){
+    
+}
